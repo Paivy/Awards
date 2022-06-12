962d00132a19627207ca013bbf6cdc67ff1a619f
Paivy <paivyeshirera@gmail.com>
2022-06-12 13:57:35 +0300

    created the signup view

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-# from .models import *
-# from .forms import SiteForm
+from .models import *
+from .forms import *
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 # from django.http import HttpResponseRedirect
 # Create your views here.
 
@@ -26,3 +31,17 @@ def home(request):
    
 #     return render(request, 'events/add_venue.html',
 #     {'form':form})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
