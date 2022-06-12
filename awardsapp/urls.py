0118fe0f39d1d03d2d8977e3666154fa641cd5c7
Paivy <paivyeshirera@gmail.com>
2022-06-12 13:15:38 +0300

    added the home path and the add site path

---
+++ awardsapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # int: numbers
+    # str: strings
+    # path: whole urls/
+    # slugs: hyphen-and_underscores_stuff
+    #UUID: universally unique identifiers
+
+     path('',views.home, name="home"),
+     path('add_site',views.add_site,name="add_site"),
+]
