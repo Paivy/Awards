384756e5394d7e3a33e6cdc8ebb2de4e6f76b014
Paivy <paivyeshirera@gmail.com>
2022-06-13 09:53:38 +0300

    registerd urls

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # int: numbers
-    # str: strings
-    # path: whole urls/
-    # slugs: hyphen-and_underscores_stuff
-    #UUID: universally unique identifiers
-
+   
      path('',views.home, name="home"),
-     path('signup/', views.signup, name='signup'),
-    #  path('add_site',views.add_site,name="add_site"),
+     path('new_project',views.new_project, name="new_project"),
+     path('profile',views.profile,name='profile'),
+     path('new_profile',views.new_profile,name = 'new_profile'),
+     path('edit/profile',views.profile_edit,name = 'edit_profile'),
+     path('search',views.search_project, name='search_results'),
+     path('project/review/(\d+)',views.project_review,name='project_review'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
