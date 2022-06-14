d588d9b0045deaa94db063474fe5e9c6ab8d0465
Paivy <paivyeshirera@gmail.com>
2022-06-14 10:55:31 +0300

    updating bootstrap.

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -77,7 +77,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awardsapp',
-    'bootstrap4',
+    'bootstrap5',
     'rest_framework.authtoken',
     'rest_framework',
 ]
